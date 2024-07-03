e45eaf01e793ce41cf506cf6441347ccd423cc0c
Guest <wjajzhahs@gmail.com>
2024-07-03 23:27:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ import connectDB from "./db/db.js";
 import userRoute from "./routes/user.routes.js"
 import noteRoute from "./routes/note.routes.js"
 import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 dotenv.config();
 
