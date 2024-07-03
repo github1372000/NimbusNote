f74c355a58a96f66a857f113e6274744516c6ed6
Guest <wjajzhahs@gmail.com>
2024-07-04 00:13:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ import cors from "cors";
 import connectDB from "./db/db.js";
 import userRoute from "./routes/user.routes.js"
 import noteRoute from "./routes/note.routes.js"
-import path from "path";
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
 dotenv.config();
@@ -17,7 +12,7 @@ const app = express();
 const Port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static(path.join(__dirname, "./client/dist")));
+
 
 app.use(
   cors({
@@ -34,9 +29,7 @@ connectDB();
 app.use("/api/v1/auth", userRoute);
 app.use("/api/v1/note", noteRoute);
 
-app.use('*', function(req,res){
-  res.sendFile(path.join(__dirname,"./client/dist/index.html"));
-})
+
 
 
 app.listen(Port, () => {
